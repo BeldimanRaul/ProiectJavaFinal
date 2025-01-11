750ddf15a5e9186b6c35d7b9fd99006ead3da6d1
BeldimanRaul <raulbeldiman4@gmail.com>
2025-01-12 00:06:18 +0200

    proiect java gui

--- src/main/java/com/example/proiectjavafinal/LoginControler.java
+++ src/main/java/com/example/proiectjavafinal/LoginControler.java
@@ -3,8 +3,11 @@ package com.example.proiectjavafinal;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class LoginControler {
 
@@ -17,6 +20,24 @@ public class LoginControler {
     @FXML
     private Button registerButton;
 
+    @FXML
+    private Label loginGresit;
 
 
+
+
+    public void logingui(ActionEvent actionEvent) throws IOException {
+          verificalogin();
+    }
+
+    private void verificalogin() throws IOException {
+        Main main = new Main();
+        if (username.getText().toString().equals("") || password.getText().equals("")) {
+            loginGresit.setText("Succes");
+            main.schimba("");
+        }
+    }
+
+    public void registergui(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/com/example/proiectjavafinal/Main.java
+++ src/main/java/com/example/proiectjavafinal/Main.java
@@ -13,17 +13,27 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main extends Application {
+
+    private static Stage stg;
+
+
+    public void schimba(String fxml) throws IOException {
+        Parent pane =FXMLLoader.load(getClass().getResource(fxml)) ;
+        stg.getScene().setRoot(pane);
+    }
     public static void main (String[] args) throws IOException {
         launch(args);
 
     }
 
     @Override
-    public void start(Stage fereastra) throws Exception {
+    public void start(Stage fereastraprincipala) throws Exception {
+        stg = fereastraprincipala;
+        fereastraprincipala.setResizable(false);
        Parent root=FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-       fereastra.setScene(new Scene(root,600,400));
-       fereastra.setTitle("APLICATIE");
-       fereastra.show();
+        fereastraprincipala.setScene(new Scene(root,600,400));
+        fereastraprincipala.setTitle("APLICATIE");
+        fereastraprincipala.show();
 
 
     }
