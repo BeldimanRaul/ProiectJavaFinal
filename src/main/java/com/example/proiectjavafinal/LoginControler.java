767d9aceaeba506ba986f53141e2697ca167978f
BeldimanRaul <raulbeldiman4@gmail.com>
2025-01-11 19:29:16 +0200

    proiect java gui

--- src/main/java/com/example/proiectjavafinal/LoginControler.java
+++ src/main/java/com/example/proiectjavafinal/LoginControler.java
@@ -1,4 +1,22 @@
 package com.example.proiectjavafinal;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginControler {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button registerButton;
+
+    public void logingui() {
+
+    }
 }
